8eaf226f80edac9b35e7f644e05e12c229d32c7c
hu dong lin <benabcd5779@gmail.com>
2020-10-13 21:39:52 +0800

    git init commit & DB setting

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_DATABASE', 'lara_0804_p');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'benabcd5779');
+define('DB_PASSWORD', 'roytbhkm1234');
